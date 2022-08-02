1f083652c0ea92a5ab002b7cfaa1ac3cb4fd8fa1
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-08-02 18:12:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    // error_reporting(0); updating error reporting setting
-    // Here is my php file
-    // phpinfo(); => PHP information function
-    // print "<strong> hello world. </strong>"; slower than echo
-    // echo "<strong> hello world. </strong>";
-    // echo "<input type=\"text\" name=\"\" placeholder=\"php boss\" id=\"\">";
-    // echo '<input type="text" name="" id="">';
+    error_reporting(0); updating error reporting setting
+    Here is my php file
+    phpinfo(); => PHP information function
+    print "<strong> hello world. </strong>"; slower than echo
+    echo "<strong> hello world. </strong>";
+    echo "<input type=\"text\" name=\"\" placeholder=\"php boss\" id=\"\">";
+    echo '<input type="text" name="" id="">';
 
 
 ?>
